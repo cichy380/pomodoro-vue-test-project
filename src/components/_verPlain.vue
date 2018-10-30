fb4e5cd33633e7cebf35d7bf9caa27f5dfc51c2e
Marcin Dobroszek <marcin.dobroszek@gmail.com>
2018-10-30 20:59:05 +0100

    Added states (play, pause, stop)

--- src/components/_verPlain.vue
+++ src/components/_verPlain.vue
@@ -4,41 +4,75 @@
     <h5 class="mb-4 text-muted">plain version</h5>
 
     <section class="jumbotron p-2">
-      <div class="timer display-4 text-center">{{ minute }}:{{ second }}</div>
-
+      <div class="timer display-4">
+        <span :class="{'alert-danger':pomodoroState==='work', 'alert-success':pomodoroState==='rest', }">{{ message }}</span>
+        {{ min }}:{{ sec }}
+      </div>
     </section>
 
     <div class="btn-group btn-group-sm" role="group">
-      <button type="button" class="btn btn-outline-primary disabled" disabled>Stop</button>
-      <button type="button" class="btn btn-outline-primary" @click="start()">Play</button>
-      <button type="button" class="btn btn-outline-primary">Pause</button>
+      <button type="button" class="btn btn-outline-primary" @click="start()" :disabled="state==='started'">Play</button>
+      <button type="button" class="btn btn-outline-primary" @click="pause()" :disabled="state!=='started'">Pause</button>
+      <button type="button" class="btn btn-outline-primary" @click="stop()" :disabled="state!=='started' && state!=='paused'">Stop</button>
     </div>
   </div>
 </template>
 
 <script>
-    const STATES = {
+    const POMODORO_STATES = {
         WORK: 'work',
         REST: 'rest'
     }
-    const WORKING_TIME = 25
-    const RESTING_TIME = 5
+    const STATES = {
+        STARTED: 'started',
+        STOPPED: 'stopped',
+        PAUSED: 'paused'
+    }
+    const WORKING_TIME = 1
+    const RESTING_TIME = 2
 
     export default {
         name: 'PomodoroPlain',
         data () {
             return {
+                state: STATES.STOPPED,
                 minute: WORKING_TIME,
                 second: 0,
-                state: STATES.WORK,
+                pomodoroState: POMODORO_STATES.WORK,
                 timestamp: 0
+            }
+        },
+        computed: {
+            message: function () {
+                return this.pomodoroState === POMODORO_STATES.WORK ? 'Work' : 'Rest'
+            },
+            min: function () {
+                return (this.minute < 10) ? `0${this.minute}` : this.minute
+            },
+            sec: function () {
+                return (this.second < 10) ? `0${this.second}` : this.second
             }
         },
         methods: {
             start () {
+                this.state = STATES.STARTED
                 this._tick()
                 this.interval = setInterval(this._tick, 1000)
             },
+
+            pause () {
+                this.state = STATES.PAUSED
+                clearInterval(this.interval)
+            },
+
+            stop () {
+                this.state = STATES.STOPPED
+                clearInterval(this.interval)
+                this.pomodoroState = POMODORO_STATES.WORK
+                this.minute = WORKING_TIME
+                this.second = 0
+            },
+
             _tick () {
                 if (this.second !== 0) {
                     this.second--
@@ -52,10 +86,10 @@
                 }
 
                 // this.minute===0 && this.second===0
-                this.state = (this.state === STATES.WORK) ? STATES.REST : STATES.WORK
+                this.pomodoroState = (this.pomodoroState === POMODORO_STATES.WORK) ? POMODORO_STATES.REST : POMODORO_STATES.WORK
 
                 // timer reset
-                if (this.state === STATES.WORK) {
+                if (this.pomodoroState === POMODORO_STATES.WORK) {
                     this.minute = WORKING_TIME
                 } else {
                     this.minute = RESTING_TIME
